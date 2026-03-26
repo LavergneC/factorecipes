dad2a7c901beeeef2f0c16c31ebcc895338c9563
Lavergne Clement <lavergne.clement@yahoo.fr>
2026-03-26 15:06:53 +0100

    First working level 2

--- main/factorecipes.py
+++ main/factorecipes.py
@@ -2,13 +2,17 @@ from collections import defaultdict
 
 type RecipeName = str
 type IngredientName = str
-type RecipeIngredients = dict[str, int]
+type RecipeIngredients = dict[IngredientName, int]
 
 
 class FactoRecipes:
     def __init__(self) -> None:
         self._recipes: dict[RecipeName, RecipeIngredients] = {}
-        self._needs = defaultdict(int)
+        self._basic_ingredients: set[IngredientName] = set()
+        self._needs_to_compute = defaultdict(int)
+
+    def add_basic_ingredient(self, name: IngredientName) -> None:
+        self._basic_ingredients.add(name)
 
     def add_recipe(self, recipe_name: str, ingredients: dict[str, int]) -> None:
         if recipe_name in self._recipes.keys():
@@ -16,18 +20,32 @@ class FactoRecipes:
         self._recipes[recipe_name] = ingredients
 
     def add_need(self, name: str, quantity: int) -> None:
-        self._needs[name] += quantity
+        self._needs_to_compute[name] += quantity
 
     def compute_needs(self) -> dict[str, int]:
-        running_needs = defaultdict(int)
+        self._running_needs = defaultdict(int)
 
-        for ingredient, quantity in self._needs.items():
-            running_needs[ingredient] += quantity
+        while len(self._needs_to_compute) != 0:
+            self._compute_current_needs()
+
+        return self._running_needs
+
+    def _compute_current_needs(self) -> None:
+        new_needs = defaultdict(int)
+
+        for ingredient, quantity in self._needs_to_compute.items():
+            self._running_needs[ingredient] += quantity
+
+            # If it's a basic ingredient nothing more to do
+            if ingredient in self._basic_ingredients:
+                continue
+
+            if ingredient not in self._recipes.keys():
+                raise RuntimeError(f"Missing recipe for '{ingredient}'")
+
             recipe = self._recipes[ingredient]
 
             for recipe_ingredient, recipe_ingredient_quantity in recipe.items():
-                running_needs[recipe_ingredient] += (
-                    recipe_ingredient_quantity * quantity
-                )
+                new_needs[recipe_ingredient] += recipe_ingredient_quantity * quantity
 
-        return running_needs
+        self._needs_to_compute = new_needs
--- tests/test_factorecipes.py
+++ tests/test_factorecipes.py
@@ -6,6 +6,9 @@ from main.factorecipes import FactoRecipes
 class TestFactoRecipes:
     def test_simple_recipe_usage(self):
         facto_recipes = FactoRecipes()
+        facto_recipes.add_basic_ingredient(name="jambon")
+        facto_recipes.add_basic_ingredient(name="beurre")
+
         facto_recipes.add_recipe(
             recipe_name="jambon beurre",
             ingredients={"jambon": 2, "beurre": 5},
@@ -34,8 +37,12 @@ class TestFactoRecipes:
                 ingredients={"jambon": 2, "beurre": 5, "pain": 1},
             )
 
-    def test_two_recipe_usage_one_level(self):
+    def test_two_recipes_usage_one_level(self):
         facto_recipes = FactoRecipes()
+        facto_recipes.add_basic_ingredient(name="jambon")
+        facto_recipes.add_basic_ingredient(name="beurre")
+        facto_recipes.add_basic_ingredient(name="fromage")
+
         facto_recipes.add_recipe(
             recipe_name="jambon beurre",
             ingredients={"jambon": 2, "beurre": 5},
@@ -54,3 +61,37 @@ class TestFactoRecipes:
             "beurre": 25,
             "fromage": 9,
         }
+
+    def test_two_recipes_usage_two_levels(self):
+        facto_recipes = FactoRecipes()
+
+        facto_recipes.add_basic_ingredient(name="jambon")
+        facto_recipes.add_basic_ingredient(name="lait")
+
+        facto_recipes.add_recipe(
+            recipe_name="jambon beurre",
+            ingredients={"jambon": 2, "beurre": 5},
+        )
+        facto_recipes.add_recipe(
+            recipe_name="beurre",
+            ingredients={"lait": 5},
+        )
+        facto_recipes.add_need(name="jambon beurre", quantity=2)
+
+        assert facto_recipes.compute_needs() == {
+            "jambon beurre": 2,
+            "jambon": 4,
+            "beurre": 10,
+            "lait": 50,
+        }
+
+    def test_missing_recipe(self):
+        facto_recipes = FactoRecipes()
+        facto_recipes.add_recipe(
+            recipe_name="beurre",
+            ingredients={"lait": 2},
+        )
+        facto_recipes.add_need(name="beurre", quantity=2)
+
+        with pytest.raises(RuntimeError, match="Missing recipe for 'lait'"):
+            facto_recipes.compute_needs()
