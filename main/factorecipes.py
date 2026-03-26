342045c87f874a346d0c757396e9a2cd7c7db86d
Lavergne Clement <lavergne.clement@yahoo.fr>
2026-03-26 12:12:36 +0100

    First working code for one level

--- main/factorecipes.py
+++ main/factorecipes.py
@@ -1,2 +1,33 @@
-def foo():
-    pass
+from collections import defaultdict
+
+type RecipeName = str
+type IngredientName = str
+type RecipeIngredients = dict[str, int]
+
+
+class FactoRecipes:
+    def __init__(self) -> None:
+        self._recipes: dict[RecipeName, RecipeIngredients] = {}
+        self._needs = defaultdict(int)
+
+    def add_recipe(self, recipe_name: str, ingredients: dict[str, int]) -> None:
+        if recipe_name in self._recipes.keys():
+            raise RuntimeError(f"The recipe '{recipe_name}' was already defined")
+        self._recipes[recipe_name] = ingredients
+
+    def add_need(self, name: str, quantity: int) -> None:
+        self._needs[name] += quantity
+
+    def compute_needs(self) -> dict[str, int]:
+        running_needs = defaultdict(int)
+
+        for ingredient, quantity in self._needs.items():
+            running_needs[ingredient] += quantity
+            recipe = self._recipes[ingredient]
+
+            for recipe_ingredient, recipe_ingredient_quantity in recipe.items():
+                running_needs[recipe_ingredient] += (
+                    recipe_ingredient_quantity * quantity
+                )
+
+        return running_needs
--- tests/test_factorecipes.py
+++ tests/test_factorecipes.py
@@ -1,2 +1,56 @@
-def test_dumb():
-    assert True
+import pytest
+
+from main.factorecipes import FactoRecipes
+
+
+class TestFactoRecipes:
+    def test_simple_recipe_usage(self):
+        facto_recipes = FactoRecipes()
+        facto_recipes.add_recipe(
+            recipe_name="jambon beurre",
+            ingredients={"jambon": 2, "beurre": 5},
+        )
+        facto_recipes.add_need(name="jambon beurre", quantity=2)
+
+        assert facto_recipes.compute_needs() == {
+            "jambon beurre": 2,
+            "jambon": 4,
+            "beurre": 10,
+        }
+
+    def test_recipe_redefinition(self):
+        facto_recipes = FactoRecipes()
+        facto_recipes.add_recipe(
+            recipe_name="jambon beurre",
+            ingredients={"jambon": 2, "beurre": 5},
+        )
+
+        with pytest.raises(
+            RuntimeError,
+            match="The recipe 'jambon beurre' was already defined",
+        ):
+            facto_recipes.add_recipe(
+                recipe_name="jambon beurre",
+                ingredients={"jambon": 2, "beurre": 5, "pain": 1},
+            )
+
+    def test_two_recipe_usage_one_level(self):
+        facto_recipes = FactoRecipes()
+        facto_recipes.add_recipe(
+            recipe_name="jambon beurre",
+            ingredients={"jambon": 2, "beurre": 5},
+        )
+        facto_recipes.add_recipe(
+            recipe_name="beurre fromage",
+            ingredients={"fromage": 3, "beurre": 5},
+        )
+        facto_recipes.add_need(name="jambon beurre", quantity=2)
+        facto_recipes.add_need(name="beurre fromage", quantity=3)
+
+        assert facto_recipes.compute_needs() == {
+            "jambon beurre": 2,
+            "beurre fromage": 3,
+            "jambon": 4,
+            "beurre": 25,
+            "fromage": 9,
+        }
